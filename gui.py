a645df92686bc6855e09e7c708649316b99553ff
Jin <jinparkhan+git@googlemail.com>
2017-04-26 18:08:26 +0100

    Added more features with UI and sync options.

--- gui.py
+++ gui.py
@@ -68,44 +68,58 @@ root.wm_resizable(width=False, height=False)
 Label(root, text="Source").grid(row=0, column=0)
 Label(root, text="Destination").grid(row=1, column=0)
 
-entry_source = Entry(root, width=100)
+entry_source = ttk.Entry(root, width=100)
 entry_source.grid(row=0, column=1)
-entry_source.insert(0, r'X:\1 Music Vid\1 Dances\1 Lessons\1 Lessons Share')
+entry_source.insert(0, r'C:/3 test file/3 programming/4 dance folder skelton - test 01/3 dance sync test - source')
 entry_partial = partial(choose_paths, entry_source)
 Button(root, text="...", command=entry_partial).grid(row=0, column=3)
 
 
-entry_destination = Entry(root, width=100)
+entry_destination = ttk.Entry(root, width=100)
 entry_destination.grid(row=1, column=1)
-entry_destination.insert(0, r'D:\OneDrive\Pictures\4 Sharing with Others\Dances')
+entry_destination.insert(0, r'C:/3 test file/3 programming/4 dance folder skelton - test 01/3 destination')
 destination_partial = partial(choose_paths, entry_destination)
 Button(root, text="...", command=destination_partial).grid(row=1, column=3)
 
 progbar = ttk.Progressbar(root, mode='indeterminate', length = 600)
 progbar.grid(row=2, columnspan=3)
 
+frame_options = Frame(root)
+frame_options.grid(row=4, column=1)
+
+frame_log = Frame(root)
 
 #Enables, or disables showing of log_box.
-log_box = Text(root)
+log_scrollbar = ttk.Scrollbar(frame_log)
+log_box = Text(frame_log, yscrollcommand = log_scrollbar.set )
+log_box.tag_configure('warning', foreground='red')
 is_there_log = False
+log_scrollbar.config(command = log_box.yview)
+
 def enable_log():
     if not is_there_log:
         global is_there_log
         is_there_log = True
-        log_box.grid(row=5, columnspan=3)
+        frame_log.grid(row=6, columnspan=3)
+        log_box.grid(row=0, columnspan=3)
+        log_scrollbar.grid(row=0, column=4, sticky=N+S)
     elif is_there_log:
         global is_there_log
         is_there_log = False
-        log_box.grid_remove()
+        frame_log.grid_remove()
 
-label_enable_copy = Label(root, text='Enable Copy')
-label_enable_copy.grid(row=3, column=1)
+label_enable_copy = Label(frame_options, text='Enable Copy')
+label_enable_copy.grid(row=0, column=1)
 var_enable_copy = IntVar()
-checkBtn_enable_copy = Checkbutton(root, variable=var_enable_copy)
-checkBtn_enable_copy.grid(row=4, column=1)
+checkBtn_enable_copy = Checkbutton(frame_options, variable=var_enable_copy)
+checkBtn_enable_copy.grid(row=0, column=2)
 
+var_combo_check_method = StringVar()
+combo_check_box = ttk.Combobox(root, textvariable=var_combo_check_method, state='readonly')
+combo_check_box['values'] = ('Name and Size', 'Name and Hash')
+combo_check_box.grid(row=5, column=1)
+combo_check_box.current(0)
 
-Button(root, text="Sync", command=run_sync).grid(row=4, column=0)
-Button(root, text="Log", command=enable_log).grid(row=4, column=2)
-Button(root, text="Quit", command=root.quit).grid(row=4, column=3)
-# Button(root, text="Dev_Test", command=clear_entry).grid(row=2, column=3)
+Button(root, text="Sync", command=run_sync).grid(row=5, column=0)
+Button(root, text="Log", command=enable_log).grid(row=5, column=2)
+Button(root, text="Quit", command=root.quit).grid(row=5, column=3)
--- utils.py
+++ utils.py
@@ -27,8 +27,8 @@ Contains logic for checking, copying files.
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 # ospath way of doing input... some sort of parasing. split method?
-original_folder = r'X:\1 Music Vid\1 Dances\1 Lessons\1 Lessons Share'
-copy_folder = r'D:\OneDrive\Pictures\4 Sharing with Others\Dances'
+original_folder = r'C:/3 test file/3 programming/4 dance folder skelton - test 01/3 dance sync test - source'
+copy_folder = r'C:/3 test file/3 programming/4 dance folder skelton - test 01/3 destination'
 
 class MyCopier(object):
 
@@ -37,6 +37,7 @@ class MyCopier(object):
         self.destinationFolder = destinationFolder
         self.contents_Source = []
         self.contents_destination = []
+        self.contents_to_sync_list = None
 
     def run(self):
         gui.log_box.insert(END, original_folder+'\n')
@@ -45,7 +46,7 @@ class MyCopier(object):
         gui.log_box.insert(END, '\n')
 
         if not self.checkPaths(self.sourceFolder, self.destinationFolder):
-            gui.log_box.insert(END, 'Exiting.\n')
+            gui.log_box.insert(END, 'Exiting.\n\n')
             sys.exit()
 
         self.contents_Source = self.get_contents(self.sourceFolder)
@@ -55,37 +56,33 @@ class MyCopier(object):
 
         folder_diff = self.check_folder_structure(self.contents_Source, self.contents_destination)
         if folder_diff:
-            gui.log_box.insert(END, 'ERROR: Folder structures are different between source and destination. \n Two folder are not compatible or please make structures same.\n')
+            gui.log_box.insert(END, 'ERROR: Folder structures are different between source and destination. \n Two folder are not compatible or please make structures same.\n\n', "warning")
             sys.exit()
         else:
             gui.log_box.insert(END, 'Great! Folder structures are same. Preceding.\n')
+        if gui.combo_check_box.get() == 'Name and Size':
+            self.contents_to_sync_list = self.new_contents_bubble_size_check(self.contents_Source, self.contents_destination)
+        elif gui.combo_check_box.get() == 'Name and Hash':
+            self.contents_to_sync_list = self.new_contents_bubble_hash(self.contents_Source, self.contents_destination)
+        #
 
 
-        #logging.info("====== self.contents_Source ===== \n".center(55) + str(self.contents_Source) + "\n\n")
-        #logging.info("====== self.contents_destination ===== \n".center(55) + str(self.contents_destination) + "\n\n")
-
-        #contents_to_sync_list = self.new_contents(self.contents_Source, self.contents_destination)
-        #contents_to_sync_list = self.new_contents_hash(self.contents_Source, self.contents_destination) #better updated way of copmaring files. #Hash is so slow.
-        #contents_to_sync_list = self.new_contents_bubble_hash(self.contents_Source, self.contents_destination)
-        contents_to_sync_list = self.new_contents_bubble_size_check(self.contents_Source, self.contents_destination)
-
-
-        if self.number_of_files(contents_to_sync_list) == 0:
-            gui.log_box.insert(END, "Done! No file needs syncing! :) \n")
+        if self.number_of_files(self.contents_to_sync_list) == 0:
+            gui.log_box.insert(END, "Done! No file needs syncing! :) \n\n")
             sys.exit()
         else:
-            # gui.log_box.insert(END, "disabled copy content for dev purposes. Please enable manually in code to copy. Maybe add tick button to enable or disable copy(?) \n")
-            self.copy_contents(contents_to_sync_list, self.contents_destination)
+            self.copy_contents(self.contents_to_sync_list, self.contents_destination)
 
-        self.number_of_files(contents_to_sync_list)
+        self.number_of_files(self.contents_to_sync_list)
         self.number_of_files(self.contents_Source)
+        gui.log_box.insert(END, '\n\n')
 
     #Not sure how I should design this. Maybe make dir if it doesn't exist? For now I'll only copy if two folders are there.
     def checkPaths(self, folder1, folder2):
         if os.path.isdir(folder1) and os.path.isdir(folder2):
             gui.log_box.insert(END, "Great! Source and Destination exists. Lets go ahead. \n")
             return True
-        gui.log_box.insert(END, "Error! Source or Destination is missing \n")
+        gui.log_box.insert(END, "Error: Source or Destination is missing \n", 'warning')
         return False
 
     def get_contents(self, an_folder):
@@ -93,99 +90,6 @@ class MyCopier(object):
         for folderName, subFolders, fileNames in os.walk(an_folder):
             list_of_contents.append((folderName, subFolders, fileNames))
         return list_of_contents
-
-    #For now lets keep it simple
-    #And assume user isn't going to create extra folder and other issues in backup folder.
-    #This method simply looks for missing content in destination folder
-    #aka
-    #What the new files to add.
-    def new_contents(self, source, destination):
-        new_content_list = copy.deepcopy(source) #To make copy of the list
-        #logging.info("====== new_content_list ===== \n".center(55) + str(new_content_list) + "\n\n")
-        logging.debug((len(source)))
-        try:
-            for i in range(len(source)):
-                logging.debug("i: {}. .".format(i))
-                for file_a in source[i][2]:
-
-                    # *** file_a_full_path = first part + \\ +file_a (use os.path to put it into list?) aka source[i][0] + "\\" + file_a
-
-
-                    logging.debug("file_a ===== a =====: {}".format(file_a))
-                    #logging.debug("source[i][2]: {}".format(source[i][2]))
-                    for file_b in destination[i][2]:
-
-                        # *** file_b_full_path = first part + \\ +file_b (use os.path to put it into list?)
-
-                        logging.debug("file_b xxxxxx b xxxxxx: {}".format(file_b))
-                        #logging.debug("destination[i][2]: {}".format(destination[i][2]))
-
-                        # replace if with...
-                        # *** if m_hash.returnHash(file_a_full_path) == m_hash.returnHash(file_b_full_path):
-
-
-                        if file_a == file_b:
-                            logging.info("From list, Removing {}\\{}".format(source[i][0], file_a))
-                            new_content_list[i][2].remove(file_a)
-            return new_content_list
-        except IndexError:
-            logging.debug("IndexError. Source or destination is empty. Content_to_sync_list will be none so next part will throw error!")
-
-    #fuuuck takes so long. Maybe crc-32 check instead?
-    def new_contents_hash(self, source, destination):
-        new_content_list = copy.deepcopy(source)  # To make copy of the list
-        # logging.info("====== new_content_list ===== \n".center(55) + str(new_content_list) + "\n\n")
-        logging.debug((len(source)))
-        try:
-            for i in range(len(source)):
-                logging.debug("i: {}. .".format(i))
-                for file_a in source[i][2]:
-                    file_a_full_path = source[i][0] + '\\' + file_a
-                    logging.debug("file_a ===== a =====: {}".format(file_a))
-                    for file_b in destination[i][2]:
-                        file_b_full_path = destination[i][0] + '\\' + file_b
-                        logging.debug("file_b xxxxxx b xxxxxx: {}".format(file_b))
-                        # replace if with...
-                        # *** if m_hash.returnHash(file_a_full_path) == m_hash.returnHash(file_b_full_path):
-
-
-                        if m_hash.returnHash(file_a_full_path) == m_hash.returnHash(file_b_full_path):
-                            logging.info("From list, Removing {}\\{}".format(source[i][0], file_a))
-                            new_content_list[i][2].remove(file_a)
-            return new_content_list
-        except IndexError:
-            logging.debug(
-                "IndexError. Source or destination is empty. Content_to_sync_list will be none so next part will throw error!")
-
-    def new_contents_bubble(self, source, destination):
-        new_content_list = copy.deepcopy(source) #To make copy of the list
-        #logging.info("====== new_content_list ===== \n".center(55) + str(new_content_list) + "\n\n")
-        logging.debug((len(source)))
-        try:
-            for i in range(len(source)):
-                logging.debug("i: {}. .".format(i))
-                #below is the search algorithm.
-                for file_a in source[i][2]:
-                    logging.debug("file_a ===== a =====: {}".format(file_a))
-                    #logging.debug("source[i][2]: {}".format(source[i][2]))
-                    first = 0
-                    last = len(destination[i][2])-1
-                    found = False
-                    while first <= last and not found:
-                        midpoint = (first+last)//2
-                        if destination[i][2][midpoint] == file_a:
-                            found = True
-                            logging.info("Bubble_sort: From list, Removing {}\\{}".format(source[i][0], file_a))
-                            new_content_list[i][2].remove(file_a)
-                        else:
-                            if file_a < destination[i][2][midpoint]:
-                                last = midpoint-1
-                            else:
-                                first = midpoint+1
-            return new_content_list
-        except IndexError:
-            logging.debug(
-                "IndexError. Source or destination is empty. Content_to_sync_list will be none so next part will throw error!")
 
     def new_contents_bubble_size_check(self, source, destination):
         new_content_list = copy.deepcopy(source) #To make copy of the list
@@ -200,25 +104,55 @@ class MyCopier(object):
                     first = 0
                     last = len(destination[i][2])-1
                     found = False
-
                     while first <= last and not found:
-                        midpoint = (first+last)//2
-
+                        midpoint = (first+last) // 2
                         file_a_full = source[i][0] + '\\' + file_a
                         file_b = destination[i][2][midpoint]
                         file_b_full = destination[i][0] + '\\' + file_b
-
-                        #If name and size are same it takes it out of new_content_list. new_content_list contains all the items to sync
+                        # If name and size are same it takes it out of new_content_list. new_content_list contains all the items to sync
                         if self.check_file_names(file_a, file_b) and self.check_file_size(file_a_full, file_b_full):
                             found = True
                             logging.info("Bubble_sort + name + size checker passed: From list, Removing {}\\{}".format(source[i][0], file_a))
                             new_content_list[i][2].remove(file_a)
-                        elif self.check_file_names(file_a, file_b) and self.check_file_size(file_a_full, file_b_full):
-                            gui.log_box.insert(END, "Uh oh... two file with same name but different size\n")
-                            gui.log_box.insert(END, "or same size but different name! Please check these files\n")
-                            gui.log_box.insert(END, file_a_full+"\n")
-                            gui.log_box.insert(END, file_b_full+"\n")
-                            sys.exit()
+                        # elif self.check_file_names(file_a, file_b) or self.check_file_size(file_a_full, file_b_full):
+                        #     gui.log_box.insert(END, "Error: Uh oh... two file with same name but different size\n", 'warning')
+                        #     gui.log_box.insert(END, "or same size but different name! Please check these files\n", 'warning')
+                        #     gui.log_box.insert(END, file_a_full+"\n")
+                        #     gui.log_box.insert(END, file_b_full+"\n\n")
+                        #     sys.exit()
+                        else:
+                            if file_a < destination[i][2][midpoint]:
+                                last = midpoint-1
+                            else:
+                                first = midpoint+1
+            return new_content_list
+        except IndexError:
+            logging.debug("IndexError. Source or destination is empty. Content_to_sync_list will be none so next part will throw error!")
+
+
+    def new_contents_bubble_hash(self, source, destination):
+        new_content_list = copy.deepcopy(source) #To make copy of the list
+        #logging.info("====== new_content_list ===== \n".center(55) + str(new_content_list) + "\n\n")
+        logging.debug((len(source)))
+        try:
+            for i in range(len(source)):
+                logging.debug("i: {}. .".format(i))
+                # below is the search algorithm.
+                for file_a in source[i][2]:
+                    logging.debug("file_a ===== a =====: {}".format(file_a))
+                    first = 0
+                    last = len(destination[i][2])-1
+                    found = False
+                    while first <= last and not found:
+                        midpoint = (first+last) // 2
+                        file_a_full = source[i][0] + '\\' + file_a
+                        file_b = destination[i][2][midpoint]
+                        file_b_full = destination[i][0] + '\\' + file_b
+                        # If name and size are same it takes it out of new_content_list. new_content_list contains all the items to sync
+                        if m_hash.returnHash(file_a_full) == m_hash.returnHash(file_b_full) and self.check_file_names(file_a, file_b):
+                            found = True
+                            logging.info("Bubble_sort + hashed {}\\{}".format(source[i][0], file_a))
+                            new_content_list[i][2].remove(file_a)
                         else:
                             if file_a < destination[i][2][midpoint]:
                                 last = midpoint-1
@@ -241,49 +175,6 @@ class MyCopier(object):
             return True
         else:
             return False
-
-    def new_contents_bubble_hash(self, source, destination):
-        new_content_list = copy.deepcopy(source)  # To make copy of the list
-        # logging.info("====== new_content_list ===== \n".center(55) + str(new_content_list) + "\n\n")
-        logging.debug((len(source)))
-        try:
-            for i in range(len(source)):
-                logging.debug("i: {}. .".format(i))
-                # below is the search algorithm.
-                for file_a in source[i][2]:
-                    logging.debug("file_a ===== a =====: {}".format(file_a))
-                    # logging.debug("source[i][2]: {}".format(source[i][2]))
-                    file_a_full_path = source[i][0] + '\\' + file_a
-
-                    first = 0
-                    last = len(destination[i][2]) - 1
-                    found = False
-
-                    while first <= last and not found:
-                        midpoint = (first + last) // 2
-                        file_b_full_path = destination[i][0] + '\\' + destination[i][2][midpoint]
-                        if m_hash.returnHash(file_a_full_path) == m_hash.returnHash(file_b_full_path):
-                            found = True
-                            logging.info(
-                                "Bubble_sort: From list, Removing {}\\{}".format(source[i][0], file_a))
-                            new_content_list[i][2].remove(file_a)
-                        else:
-                            if file_a < destination[i][2][midpoint]:
-                                last = midpoint - 1
-                            else:
-                                first = midpoint + 1
-
-                    """
-                    for file_b in destination[i][2]:
-                        logging.debug("file_b xxxxxx b xxxxxx: {}".format(file_b))
-                        #logging.debug("destination[i][2]: {}".format(destination[i][2]))
-                        if file_a == file_b:
-                            logging.info("From list, Removing {}\\{}".format(source[i][0], file_a))
-                            new_content_list[i][2].remove(file_a)
-                    """
-            return new_content_list
-        except IndexError:
-            logging.debug("IndexError. Source or destination is empty. Content_to_sync_list will be none so next part will throw error!")
 
     # ospath way of doing here
     def copy_contents(self, sync_list, destination):
